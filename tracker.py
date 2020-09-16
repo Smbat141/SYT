de08aef6ed42ed7010ec782a84ed1111b6dac916
smbat1996 <smbat1996paloyan@gmail.com>
2020-09-16 17:39:50 +0400

    set random activity

--- tracker.py
+++ tracker.py
@@ -8,16 +8,21 @@ mouse = Controller()
 
 
 class Tracker:
+    def __init__(self):
+        self.random_number = self.get_random_number()
+
     def track(self):
         start_time = datetime.now()
-        number_list = [7, 6, 8, 7, 6, 8, 7, 6, 8, 7, 6, 8]
-        random_number = random.choice(number_list)
-        if start_time.minute % 10 < random_number:
+
+        # select random time for press space
+        space_pressing_time = random.choice(
+            [0.1, 1, 0.9, 1.3, 0.9, 0.9, 0.1, 0.2, 0.9, 0.3, 0.4, 0.9, 0.5, 0.9, 0.8, 1.3])
+
+        if start_time.minute % 10 < self.random_number:
             pyautogui.press('space')
-            threading.Timer(0000000.1, self.track).start()
+            threading.Timer(space_pressing_time, self.track).start()
         else:
-            start_after_this_minutes = (10 - random_number) * 60
-            print(start_after_this_minutes)
+            start_after_this_minutes = (10 - self.random_number) * 60
             self.wait_and_restart(start_after_this_minutes)
 
     def press_button(self, a, b):
@@ -45,8 +50,17 @@ class Tracker:
     def start(self):
         # click git icon
         threading.Timer(2, self.press_button(56, 818)).start()
+
         # click git input
         threading.Timer(5, self.press_button(537, 778)).start()
 
     def wait_and_restart(self, seconds):
+        self.random_number = self.get_random_number()
         threading.Timer(seconds, self.press_button(537, 778)).start()
+
+    @staticmethod
+    def get_random_number():
+        number_list = [7, 6, 8, 7, 6, 8, 7, 6, 8, 7, 6, 8]
+        random_number = random.choice(number_list)
+
+        return random_number
