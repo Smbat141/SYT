f1a39228913ae791834769f4b23ad0e7d1b01faa
smbat1996 <smbat1996paloyan@gmail.com>
2020-09-24 18:26:54 +0400

    random scroll when change the tab

--- script.py
+++ script.py
@@ -10,26 +10,26 @@ import pyautogui
 import random
 from pynput.mouse import Listener, Button, Controller
 
-# from tracker import Tracker
+from tracker import Tracker
+
+x = Tracker()
+x.start()
+
+# mouse = Controller()
+# mouse.position = (588, 872)
+# mouse.press(Button.left)
+# mouse.release(Button.left)
 #
-# x = Tracker()
-# x.start()
-
-mouse = Controller()
-mouse.position = (588, 872)
-mouse.press(Button.left)
-mouse.release(Button.left)
-
-
-def test():
-    mouse.position = (700, 450)
-    mouse.press(Button.left)
-    mouse.release(Button.left)
-    scroll_steps = random.randint(-100, 50)
-
-    mouse.scroll(0, scroll_steps)
-
-    return True
-
-x = threading.Timer(2, test).start()
-print(x)
+#
+# def test():
+#     mouse.position = (700, 450)
+#     mouse.press(Button.left)
+#     mouse.release(Button.left)
+#     scroll_steps = random.randint(-100, 50)
+#
+#     mouse.scroll(0, scroll_steps)
+#
+#     return True
+#
+# x = threading.Timer(2, test).start()
+# print(x)
--- tracker.py
+++ tracker.py
@@ -1,6 +1,7 @@
 import threading
 import pyautogui
 import random
+import time
 from datetime import datetime
 from pynput.mouse import Listener, Button, Controller
 
@@ -15,7 +16,8 @@ class Tracker:
         start_time = datetime.now()
 
         # select random time for press space
-        space_pressing_time = random.choice([00.1, 00.9, 00.9, 00.9, 00.1, 00.2, 00.9, 00.3, 00.4, 00.9, 00.5, 00.9, 00.8])
+        space_pressing_time = random.choice(
+            [00.1, 00.9, 00.9, 00.9, 00.1, 00.2, 00.9, 00.3, 00.4, 00.9, 00.5, 00.9, 00.8])
 
         if start_time.minute % 10 < self.random_number:
             pyautogui.press('space')
@@ -25,61 +27,55 @@ class Tracker:
             self.wait_and_restart(start_after_this_minutes)
 
     def press_button(self, a, b):
-        def wrapper():
-            if a == 537:
-
-                is_mouse_clicked = self.mouse_click(a, b)
-
-                if is_mouse_clicked:
-                    is_tab_changed = self.change_tab()
-
-                if is_tab_changed:
-                    print('scroll now')
-
-                #
-                # threading.Timer(1, self.track).start()
-
+        if a == 537:
+            time.sleep(1)
+            self.change_tab()
+            time.sleep(1)
+            self.mouse_click(700, 450)
+            time.sleep(1)
+            self.random_mouse_scroll()
+            time.sleep(1)
             self.mouse_click(a, b)
-
-        return wrapper
+            time.sleep(1)
+            self.track()
 
     def start(self):
-        # click git icon
-        # threading.Timer(2, self.press_button(56, 818)).start()
-        if self.mouse_click(56, 818):
-             self.mouse_click(700, 399)
-
-        # click git input
-        # threading.Timer(5, self.press_button(537, 778)).start()
+        self.mouse_click(642, 868)
+        time.sleep(1)
+        pyautogui.hotkey('shift', 'esc')
+        time.sleep(1)
+        self.mouse_click(56, 818)
+        time.sleep(1)
+        self.press_button(537, 778)
 
     def wait_and_restart(self, seconds):
+        time.sleep(seconds)
         self.random_number = self.get_random_number()
-        threading.Timer(seconds, self.press_button(537, 778)).start()
+        self.press_button(537, 778)
 
     @staticmethod
     def get_random_number():
-        number_list = [7, 8, 9]
+        number_list = [6, 7, 8]
         random_number = random.choice(number_list)
 
         return random_number
 
-    def mouse_click(self, x, y):
-        try:
-            mouse.position = (x, y)
-            mouse.press(Button.left)
-            mouse.release(Button.left)
-            return True
-        except:
-            return False
+    @staticmethod
+    def mouse_click(x, y):
+        mouse.position = (x, y)
+        mouse.press(Button.left)
+        mouse.release(Button.left)
 
-    def change_tab(self):
-        try:
-            print(f'start in {str(datetime.now())}')
-            # select random tab
-            pyautogui.keyDown('ctrl')
-            for i in range(0, random.randrange(1, 8)):
-                pyautogui.press('tab')
-            pyautogui.keyUp('ctrl')
-            return True
-        except:
-            return False
+    @staticmethod
+    def random_mouse_scroll():
+        scroll_steps = random.randint(-100, 50)
+        mouse.scroll(0, scroll_steps)
+
+    @staticmethod
+    def change_tab():
+        print(f'start in {str(datetime.now())}')
+        # select random tab
+        pyautogui.keyDown('ctrl')
+        for i in range(0, random.randrange(1, 8)):
+            pyautogui.press('tab')
+        pyautogui.keyUp('ctrl')
