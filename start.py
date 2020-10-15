f6145773ebc6eb042f3f01f570b17dc821d56499
smbat1996 <smbat1996paloyan@gmail.com>
2020-10-15 20:31:07 +0400

    create class for validate Linux and windows

--- start.py
+++ start.py
@@ -16,64 +16,11 @@ import subprocess
 import shelve
 from tracker import Tracker
 from bcolors import bcolors
+from validator import ValidateLinux
 
-argument_value = ''
-end = False
-if len(sys.argv) > 1:
-    if sys.argv[1][:9] == '--update=':
-        argument_value = sys.argv[1][9:]
-    elif sys.argv[1][:6] == '--end=':
-        end = sys.argv[1][6:]
+validate_linux = ValidateLinux()
+validate_linux.check_paths()
 
-        if end.isdigit():
-            end = int(end)
-        else:
-            print('Invalid argument value')
-            exit()
-    else:
-        print('option is not exist')
-        exit()
+# syt = Tracker()
+# syt.start(validate_linux.end)
 
-with shelve.open('database') as db:
-
-    if argument_value != '' and argument_value not in db.keys():
-        print('Invalid argument value')
-
-    if db.get('storm_path', False) and (False if argument_value == 'storm_path' else True):
-        print(f"storm_path is a{bcolors.OKGREEN} {db['storm_path']}{bcolors.ENDC} \U0001F44D")
-    else:
-        storm_path = input('set phpstorm starting file path (phpstorm.sh or phpstorm.bat) - ')
-        db['storm_path'] = storm_path
-
-    if db.get('storm_default_opening_file_path', False) and (False if argument_value == 'storm_default_opening_file_path' else True):
-        print(f"storm_default_opening_file_path is a{bcolors.OKGREEN} {db['storm_default_opening_file_path']}{bcolors.ENDC} \U0001F44D")
-    else:
-        default_opening_file_path_text = "set the path to the project file you need to open every time\n" \
-                                         "for example /home/home/projects/my_project/composer.json - "
-
-        default_opening_file_path = input(default_opening_file_path_text)
-        db['storm_default_opening_file_path'] = default_opening_file_path
-
-    if db.get('tracker_app_starting_file_path', False) and (False if argument_value == 'tracker_app_starting_file_path' else True):
-        print(f"tracker_app_starting_file_path is a{bcolors.OKGREEN} {db['tracker_app_starting_file_path']}{bcolors.ENDC} \U0001F44D")
-    else:
-        tracker_app_starting_file_path_text = "set the path to the tracker file you need to close when finished\n"\
-                                              "for example in Linux Ubuntu " \
-                                              "/home/home/Hubstaff/HubstaffClient.bin.x86_64 - "
-
-        tracker_app_starting_file_path = input(tracker_app_starting_file_path_text)
-
-        db['tracker_app_starting_file_path'] = tracker_app_starting_file_path
-
-    if db.get('sleep_command', False) and (False if argument_value == 'sleep_command' else True):
-        print(f"sleep_command is a{bcolors.OKGREEN} {db['sleep_command']}{bcolors.ENDC} \U0001F44D")
-
-    else:
-        sleep_command = input('set sleep command - ')
-        db['sleep_command'] = sleep_command
-
-    print('Cool! \U0001F60E')
-    print(f'{bcolors.FAIL}if You want to update path use --update=<variable_name> option{bcolors.ENDC}')
-
-syt = Tracker()
-syt.start(end)
---
+++ validator.py
@@ -0,0 +1,72 @@
+import sys
+import shelve
+from bcolors import bcolors
+
+
+class ValidateLinux:
+
+    def __init__(self):
+        self.argument_value = False
+        self.end = False
+
+        if len(sys.argv) > 1:
+            if sys.argv[1][:9] == '--update=':
+                self.argument_value = sys.argv[1][9:] if sys.argv[1][9:] else '1'
+            elif sys.argv[1][:6] == '--end=':
+                self.end = sys.argv[1][6:]
+
+                if self.end.isdigit():
+                    self.end = int(self.end)
+                else:
+                    print('Invalid argument value')
+                    exit()
+            else:
+                print('option is not exist')
+                exit()
+
+    def check_paths(self):
+        with shelve.open('database') as db:
+
+            if self.argument_value and self.argument_value not in db.keys():
+                print('Invalid argument value')
+                exit()
+
+            if db.get('storm_path', False) and (False if self.argument_value == 'storm_path' else True):
+                print(f"storm_path is a{bcolors.OKGREEN} {db['storm_path']}{bcolors.ENDC} \U0001F44D")
+            else:
+                storm_path = input('set phpstorm starting file path (phpstorm.sh or phpstorm.bat) - ')
+                db['storm_path'] = storm_path
+
+            if db.get('storm_default_opening_file_path', False) and (
+                    False if self.argument_value == 'storm_default_opening_file_path' else True):
+                print(
+                    f"storm_default_opening_file_path is a{bcolors.OKGREEN} {db['storm_default_opening_file_path']}{bcolors.ENDC} \U0001F44D")
+            else:
+                default_opening_file_path_text = "set the path to the project file you need to open every time\n" \
+                                                 "for example /home/home/projects/my_project/composer.json - "
+
+                default_opening_file_path = input(default_opening_file_path_text)
+                db['storm_default_opening_file_path'] = default_opening_file_path
+
+            if db.get('tracker_app_starting_file_path', False) and (
+                    False if self.argument_value == 'tracker_app_starting_file_path' else True):
+                print(
+                    f"tracker_app_starting_file_path is a{bcolors.OKGREEN} {db['tracker_app_starting_file_path']}{bcolors.ENDC} \U0001F44D")
+            else:
+                tracker_app_starting_file_path_text = "set the path to the tracker file you need to close when finished\n" \
+                                                      "for example in Linux Ubuntu " \
+                                                      "/home/home/Hubstaff/HubstaffClient.bin.x86_64 - "
+
+                tracker_app_starting_file_path = input(tracker_app_starting_file_path_text)
+
+                db['tracker_app_starting_file_path'] = tracker_app_starting_file_path
+
+            if db.get('sleep_command', False) and (False if self.argument_value == 'sleep_command' else True):
+                print(f"sleep_command is a{bcolors.OKGREEN} {db['sleep_command']}{bcolors.ENDC} \U0001F44D")
+
+            else:
+                sleep_command = input('set sleep command - ')
+                db['sleep_command'] = sleep_command
+
+            print('Cool! \U0001F60E')
+            print(f'{bcolors.FAIL}if You want to update path use --update=<variable_name> option{bcolors.ENDC}')
